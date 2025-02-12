4015203a240cd61c396bfe3c4bd8ce9c60055726
Kamini Pise <kaminipise0035@gmail.com>
2025-02-12 12:03:38 +0530

    changed script

--- AssignmentFramework/src/test/java/myOrganization/OrganizationTest.java
+++ AssignmentFramework/src/test/java/myOrganization/OrganizationTest.java
@@ -3,8 +3,7 @@ package myOrganization;
 public class OrganizationTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("made changes");
 	}
 
 }
--- AssignmentFramework/src/test/java/mycontacts/contactsTest.java
+++ AssignmentFramework/src/test/java/mycontacts/contactsTest.java
@@ -3,8 +3,7 @@ package mycontacts;
 public class contactsTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("made changes");
 	}
 
 }
